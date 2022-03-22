ed83dd5a9b74154c9198fdb07ec5a435b8fd32f0
Himik220 <1703_ptz@mail.ru>
2022-03-22 22:44:02 +0300

    Initial Commit

---
+++ SwiftUILesson/SwiftUILessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILessonApp.swift
+//  SwiftUILesson
+//
+//  Created by Leonid Shchipkov on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
